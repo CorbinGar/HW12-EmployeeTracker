563c7aefddab64b3d30a453c1a1d1022ce6305e7
Corbin Garner <Corbingar@gmail.com>
2021-05-04 23:18:05 -0700

    added view employees function

--- server.js
+++ server.js
@@ -62,7 +62,15 @@ mainMenu = async () => {
 
 
 const viewEmployees = async () => {
+    const [rows] = await connection.execute(
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name department, r.salary, CONCAT(m.first_name, " ", m.last_name)  manager
+        FROM employee e
+        LEFT JOIN employee m ON e.manager_id = m.id
+        JOIN role r ON e.role_id = r.id
+        JOIN department d ON r.department_id = d.id
+        ORDER BY e.id`);
 
+    return rows;
 }
 
 const insertEmployee = async () => {
